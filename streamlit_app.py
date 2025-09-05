a6ed67bbb1a8de08dee58df318008f23c0fec7be
Petru-Octavian-Cretu <petrucretu03@yahoo.com>
2025-09-05 15:36:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,3 +76,5 @@ if not pending_orders_df.empty:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
